725c563d93bfc2e27cc8eb75642aca6dad5c8023
Caique <asapcaique7@gmail.com>
2024-02-09 23:55:11 -0300

    another

--- src/javaaprimoring/aprimoring.java
+++ src/javaaprimoring/aprimoring.java
@@ -1,15 +1,32 @@
 package javaaprimoring;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalDateTime;
+
 public class aprimoring {
-     public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-         int num = 7;
+        int num = 7;
 
-         if(num % 2 == 0) {
+        if (num % 2 == 0) {
             System.out.println("par");
-         }else{
-             System.out.println("ímpar");
+        } else {
+            System.out.println("ímpar");
+        }
 
-         }
-     }
-}
+        seeBirthday();
+    }
+
+         static void seeBirthday() throws ParseException{
+            String nome = "Caique";
+            Date aniversario = new SimpleDateFormat("dd/MM/yyyy").parse("13/07/2004");
+
+            if (aniversario.equals(java.sql.Date.valueOf(LocalDateTime.now().toLocalDate()))) {
+                System.out.println("te amo" + nome + "parabéns");
+            } else {
+                System.out.println("sai fora" + " " + nome);
+            }
+        }
+    }
