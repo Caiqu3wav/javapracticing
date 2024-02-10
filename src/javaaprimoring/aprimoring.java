3f5f42e729271f74f5f1682bd06fc7c51c79107b
Caique <asapcaique7@gmail.com>
2024-02-09 23:07:52 -0300

    só pa inicia

---
+++ src/javaaprimoring/aprimoring.java
@@ -0,0 +1,15 @@
+package javaaprimoring;
+
+public class aprimoring {
+     public static void main(String[] args) {
+
+         int num = 7;
+
+         if(num % 2 == 0) {
+            System.out.println("par");
+         }else{
+             System.out.println("ímpar");
+
+         }
+     }
+}
